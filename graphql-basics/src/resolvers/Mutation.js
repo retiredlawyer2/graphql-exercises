c911265a594ca6886dec602b1892ad20301f7b7c
Hasan <retiredlawyyer@gmail.com>
2021-12-05 00:24:09 +0300

    day three part 5

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -82,26 +82,39 @@ const Mutation = {
 
         db.posts.push(post)
         
-        post.published && pubsub.publish('post', { post })
+        post.published && pubsub.publish('post', { 
+            post: {
+                mutation: 'CREATED',
+                data: post
+            }
+         })
 
         return post
     },
-    deletePost(parent, args, { db }, info) {
+    deletePost(parent, args, { db, pubsub }, info) {
         const postIndex = db.posts.findIndex(post => post.id === args.id)
 
         if (postIndex === -1) {
             throw new Error('Post not found')
         }
 
-        const deletedPosts = db.posts.splice(postIndex, 1)
+        const [post] = db.posts.splice(postIndex, 1)
 
         db.comments = db.comments.filter(comment => comment.post !== args.id)
 
-        return deletedPosts[0]
+        post.published && pubsub.publish('post', {
+            post: {
+                mutation: 'DELETED',
+                data: post
+            }
+        })
+
+        return post
     },
-    updatePost(parent, args, { db }, info) {
+    updatePost(parent, args, { db, pubsub }, info) {
         const { id, data } = args
         const post = db.posts.find(post => post.id === id)
+        const originalPost = { ...post }
 
         if (!post) {
             throw new Error('Post not found')
@@ -117,6 +130,29 @@ const Mutation = {
 
         if (typeof data.published === 'boolean') {
             post.published = data.published
+
+            if (originalPost.published && !post.published) { // DELETED
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'DELETED',
+                        data: originalPost
+                    }
+                })
+            } else if (!originalPost.published && post.published) { // CREATED
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'CREATED',
+                        data: post
+                    }
+                })
+            }
+        } else if (post.published) { // UPDATED
+            pubsub.publish('post', {
+                post: {
+                    mutation: 'UPDATED',
+                    data: post
+                }
+            })
         }
 
         return post
@@ -135,22 +171,34 @@ const Mutation = {
         }
 
         db.comments.push(comment)
-        pubsub.publish(`comment ${args.data.post}`, { comment })
+        pubsub.publish(`comment ${args.data.post}`, {
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
+        })
 
         return comment
     },
-    deleteComment(parent, args, { db }, info) {
+    deleteComment(parent, args, { db, pubsub }, info) {
         const commentIndex = db.comments.findIndex(comment => comment.id === args.id)
 
         if (commentIndex === -1) {
             throw new Error('Comment not found')
         }
 
-        const deletedComments = db.comments.splice(commentIndex, 1)
+        const [comment] = db.comments.splice(commentIndex, 1)
 
-        return deletedComments[0]
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: comment
+            }
+        })
+
+        return comment
     },
-    updateComment(parent, args, { db }, info) {
+    updateComment(parent, args, { db, pubsub }, info) {
         const { id, data } = args
         const comment = db.comments.find(comment => comment.id === id)
 
@@ -161,6 +209,13 @@ const Mutation = {
         if (typeof data.text === 'string') {
             comment.text = data.text
         }
+
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        })
 
         return comment
     }
