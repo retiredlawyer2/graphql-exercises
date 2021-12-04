92bd44c9ca95047c1cf4efa3d058701ea686f5e1
Hasan <retiredlawyyer@gmail.com>
2021-12-05 00:38:08 +0300

    day three part 6

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,16 +1,4 @@
 const Subscription = {
-    count: {
-        subscribe(parent, args, { pubsub }, info) {
-            let count = 0
-
-            setInterval(() => {
-                count++
-                pubsub.publish(`count`, { count })
-            }, 2000);
-
-            return pubsub.asyncIterator('count')
-        }
-    },
     comment: {
         subscribe(parent, { postId }, { db, pubsub }, info) {
             const post = db.posts.find(post => post.id === postId && post.published)
